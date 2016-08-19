4e58ab3b588bae2b87c10c73dc6e0793f09a9eca
Kv Manohar <kvmanohar22@gmail.com>
2016-08-19 15:55:21 +0530

    Adding the feature of saving the image data to file

--- Laptop/practise/video_server.py
+++ Laptop/practise/video_server.py
@@ -4,6 +4,7 @@ import struct
 from PIL import Image
 import cv2
 import numpy as np
+import time
 
 #declare the host and the port 
 host = '0.0.0.0'
@@ -23,6 +24,10 @@ server_socket.listen(0)
 connection = server_socket.accept()[0].makefile('rb')
 #print 'connected to the client @: ' + addr[0] + ' : ' + addr[1]
 frame = 1
+start = time.time()
+
+#data to be saved in the file
+final_image_data = np.zeros((1, 38400))
 
 try:
 	while True:
@@ -38,11 +43,21 @@ try:
 		#print 'Image dimensions are: %dx%d', image.size
 		#print image.mode, image.format
 		#print 'Saving image into ~/Git/AutoCar/data: '
-        	#image.show()
+        #image.show()
 
 		#convert stream data into numpy arrays
 		img_array = np.asarray(bytearray(image_stream.read()), dtype=np.uint8)
 		image = cv2.imdecode(img_array, 0)
+
+		#create a temporary image array
+		temp_image_mat = image[120:240, :]
+		temp_image_arr = temp_image_mat.reshape((1, 38400))
+
+		#stack the data 
+		final_image_data = np.vstack((final_image_data, temp_image_arr))
+
+		print 'dimensions are :', image.shape #320x240
+		#print 'size of the image is : ', image.size 
 
 		#save the images in the folder
 		cv2.imwrite('frame{:>05}.jpg'.format(frame), image)
@@ -54,9 +69,11 @@ try:
 
 
 finally:
+	#write the entire image data into a file
+	np.savez('./../Training_data/stream_data.npz', train=final_image_data)
+	print 'Total time taken is: ' + str(time.time()-start)
 	connection.close()
 	server_socket.close()
-
 
 
 
